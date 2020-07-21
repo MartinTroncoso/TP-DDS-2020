49a5b64a1354eb683c22e5050b331af2856b179a
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-07-21 18:24:15 -0300

    Subo Test de egresos actualizado con un test de bloqueo de nuevo egreso.

--- src/test/java/dds/gesoc/model/EgresosTest.java
+++ src/test/java/dds/gesoc/model/EgresosTest.java
@@ -1,10 +1,19 @@
 package dds.gesoc.model;
 
 import dds.gesoc.model.egresos.*;
+import dds.gesoc.model.organizaciones.BloqueoAgregarEntidadBase;
+import dds.gesoc.model.organizaciones.BloqueoNuevosEgresos;
+import dds.gesoc.model.organizaciones.Categoria;
+import dds.gesoc.model.organizaciones.Entidad;
+import dds.gesoc.model.organizaciones.EntidadBase;
+import dds.gesoc.model.organizaciones.EntidadJuridica;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import dds.gesoc.exceptions.BloquearEgresoException;
+import dds.gesoc.exceptions.BloquearEntidadBaseEnJuridicaException;
 import dds.gesoc.exceptions.DniOCuitInvalidoException;
 import dds.gesoc.exceptions.ValidarTarjetaException;
 
@@ -17,10 +26,16 @@ public class EgresosTest {
 	private Item tomate;
 	private Item cheddar;
 	private Item cebolla;
+	private Item queso;
 	private static String DNI_CERO = "0";
 	private static String DNI_NEGATIVO = "-1";
 	private static String DNI_CON_NUEVE_CIFRAS = "100000000";
 	private static String CUIT_INCORRECTO = "20-12345-678-0";
+	private Entidad entidadJuridica;
+	private EntidadBase entidadBase;
+	private Categoria categoria;
+	private BloqueoNuevosEgresos bloqueoNuevosEgresos;
+	private BloqueoAgregarEntidadBase bloqueoAgregarEntidadBase;
 
 	private DatosEgreso datosEgreso;
     @Before
@@ -33,17 +48,24 @@ public class EgresosTest {
     	tomate = new Item("tomate", 150.00);
     	cheddar = new Item("cheddar", 70.00);
     	cebolla = new Item("cebolla", 175.00);
+    	queso = new Item("queso",26.00);
 
     	datosEgreso = new DatosEgreso(burguerKing, tarjeta);
 
-        egreso = new Egreso(datosEgreso, null, 0, null);
+        egreso = new Egreso(datosEgreso, null, 0, null, null);
+        
+        categoria = new Categoria("ONG");
+        entidadJuridica = new EntidadJuridica("Hermanos Scott",categoria, 200.0,"IBM","20-42498956-2","6600");
+        entidadBase = new EntidadBase("Hermanos perez",categoria, 200.0, "Venta de autos");
+        bloqueoNuevosEgresos = new BloqueoNuevosEgresos(egreso);
+        bloqueoAgregarEntidadBase = new BloqueoAgregarEntidadBase(entidadBase);
     }
     
     @Test
     public void aniadirItems() {
     	egreso.agregarItem(lechuga);
     	egreso.agregarItem(cheddar);
-    	Assert.assertEquals(270.00, egreso.valorTotal().getCantidad(), 0);
+    	Assert.assertEquals(270.00, egreso.valorTotal().getMonto(), 0);
     }
     
     @Test(expected = DniOCuitInvalidoException.class)
@@ -79,5 +101,13 @@ public class EgresosTest {
     @Test
     public void sabiendoElValorDeUnItem() {
     	Assert.assertEquals(200.00, lechuga.getValor(), 0);
+    }
+    
+    @Test(expected = BloquearEgresoException.class)
+    public void egresoBloqueadoException(){
+    	categoria.agregarReglaDeNegocio(bloqueoNuevosEgresos);
+    	egreso.agregarItem(cebolla);
+    	egreso.agregarItem(queso);
+    	entidadJuridica.setListaEgresos(egreso);
     }
 }
