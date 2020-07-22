8ebfe7a0b7918f09f10053c6406967dd8ef018f1
mari967 <mar.y.sol.240@gmail.com>
2020-07-22 00:50:59 -0300

    Borrado un comentario en el main

--- src/main/java/dds/gesoc/Main/Main.java
+++ src/main/java/dds/gesoc/Main/Main.java
@@ -44,42 +44,7 @@ public class Main {
     
     public static void main(String[] args) {
 
-        /*proveedorA = new Proveedor("Casa 9 de Julio");
-        proveedorBarato = new Proveedor("jardineria");
-        proveedorC = new Proveedor("garbarino");
-        presupuestoDocumento = new Documento("Presupuesto", 44);
-        documentoFactura = new Documento("A", 55);
-        unMedioDePago = new MedioPago(TipoMedioPago.EFECTIVO);
-        datosEgreso = new DatosEgreso(proveedorA, documentoFactura,unMedioDePago);
-
-        moneda = new Moneda("ARS", "plata", "$");
-
-        egreso = new Egreso(datosEgreso, moneda, 2, new CriterioSeleccionProveedorMenorValor());
-        egresoInvalido = new Egreso(datosEgreso, moneda, 5, new CriterioSeleccionProveedorMenorValor());
-
-        presupuestoCasa9DeJulio = new Presupuesto(proveedorA, presupuestoDocumento, moneda, "Vende cerca", egreso);
-        presupuestoJardinBarato = new Presupuesto(proveedorBarato, presupuestoDocumento, moneda, "Vende barato", egreso);
-        presupuestoGarbarino = new Presupuesto(proveedorC, presupuestoDocumento, moneda, "Vende caro", egreso);
-
-
-        parrillaCara = new Item("parrilla", 35000);
-        parrillaEconomica = new Item("parilla", 1200);
-        parrillaMedia = new Item("parrilla", 12500);
-        presupuestoCasa9DeJulio.agregarItem(parrillaMedia);
-        presupuestoJardinBarato.agregarItem(parrillaEconomica);
-        presupuestoGarbarino.agregarItem(parrillaCara);
-
-
-
-        //Usuarios
-        empresaEjemplo = new Empresa("mc Donaldo", "Arcos Dorados SRL", "27-12345678-1",
-                "Av. Corrientes 5600", new SectorServicios(), 150000);
-        primerRevisor = new Usuario("juancito", "12Q4ldldldld", TipoUsuario.ESTANDAR, empresaEjemplo);
-        segundoRevisor = new Usuario("maarilulis", "ppp1utututut", TipoUsuario.ESTANDAR, empresaEjemplo);
-
-        //while() {
-        //}
-        */
+        
         repo = RepoEgresos.getInstance();
 
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
