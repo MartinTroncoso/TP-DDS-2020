02f938eaa19d6b17ba250340dc8d067008dd3701
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-09-22 23:58:31 -0300

    Subo la clase Usuario mapeada. Falta la tabla MensajeSobreValidación

--- src/main/java/dds/gesoc/model/usuarios/Usuario.java
+++ src/main/java/dds/gesoc/model/usuarios/Usuario.java
@@ -5,15 +5,30 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Stack;
 
+import javax.persistence.*;
+
+import dds.gesoc.entities.EntidadPersistente;
 import dds.gesoc.model.egresos.ResultadoValidacion;
 import dds.gesoc.model.organizaciones.Entidad;
 
-public class Usuario {
+@Entity
+@Table(name="usuario")
+public class Usuario extends EntidadPersistente{
 	
+	@Column
 	private String nombreUsuario;
+	
+	@Column
 	private String contrasenia;
+	
+	@Enumerated
 	private TipoUsuario tipoUsuario;
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name="id_entidad", referencedColumnName = "id")
 	private Entidad entidad;
+	
+	@Transient
 	private Stack<ResultadoValidacion> bandejaDeMensajes = new Stack<>();
 	
 	public Usuario(String nombreUsuario, String contrasenia, TipoUsuario tipoUsuario, Entidad entidad) {
