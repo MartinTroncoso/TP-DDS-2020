cb9d4def347e66baf9e5455ca4d4cb586b68c9c8
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:41:54 -0300

    Delete ContraseniaConLongitudCortaException.java

--- src/java/dds/gesoc/exceptions/ContraseniaConLongitudCortaException.java
+++
@@ -1,8 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class ContraseniaConLongitudCortaException extends RuntimeException {
-
-	public ContraseniaConLongitudCortaException(String message) {
-		super(message);
-	}
-}
