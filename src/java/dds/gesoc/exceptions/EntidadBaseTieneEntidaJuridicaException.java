5e768dd1b6cb081b27bf3e5a23de9f539fbf133e
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:42:22 -0300

    Delete EntidadBaseTieneEntidaJuridicaException.java

--- src/java/dds/gesoc/exceptions/EntidadBaseTieneEntidaJuridicaException.java
+++
@@ -1,9 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class EntidadBaseTieneEntidaJuridicaException extends RuntimeException {
-
-    public EntidadBaseTieneEntidaJuridicaException(String mensaje) {
-        super(mensaje);
-    }
-
-}
