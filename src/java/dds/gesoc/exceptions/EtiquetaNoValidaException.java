5e2f37ed6087d1864cd756ece5544bd88a656b7d
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:42:32 -0300

    Delete EtiquetaNoValidaException.java

--- src/java/dds/gesoc/exceptions/EtiquetaNoValidaException.java
+++
@@ -1,7 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class EtiquetaNoValidaException extends RuntimeException {
-    public EtiquetaNoValidaException(String mensaje) {
-        super(mensaje);
-    }
-}
