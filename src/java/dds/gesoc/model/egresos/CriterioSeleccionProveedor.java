1534ff1d3f101ba7cd5a60c058ca40adc83a5ecc
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:39:06 -0300

    Delete CriterioSeleccionProveedor.java

--- src/java/dds/gesoc/model/egresos/CriterioSeleccionProveedor.java
+++
@@ -1,7 +0,0 @@
-package dds.gesoc.model.egresos;
-
-import java.util.List;
-
-public interface CriterioSeleccionProveedor {
-	public Proveedor seleccionarProveedor(List<Presupuesto> listaDePresupuestos);
-}
