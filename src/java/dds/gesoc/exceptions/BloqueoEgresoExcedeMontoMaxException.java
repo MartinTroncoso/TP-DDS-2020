85100dbc53e9fd996327d24a47b558f1385d7b7d
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:41:39 -0300

    Delete BloqueoEgresoExcedeMontoMaxException.java

--- src/java/dds/gesoc/exceptions/BloqueoEgresoExcedeMontoMaxException.java
+++
@@ -1,7 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class BloqueoEgresoExcedeMontoMaxException extends RuntimeException{
-	public BloqueoEgresoExcedeMontoMaxException(String message) {
-		super(message);
-	}
-}
