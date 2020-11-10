c40f074a65122ce9baee12277acbeff8f0537b0b
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:42:39 -0300

    Delete EtiquetaYaExistenteException.java

--- src/java/dds/gesoc/exceptions/EtiquetaYaExistenteException.java
+++
@@ -1,7 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class EtiquetaYaExistenteException extends RuntimeException {
-    public EtiquetaYaExistenteException (String mensaje){
-        super(mensaje);
-    }
-}
