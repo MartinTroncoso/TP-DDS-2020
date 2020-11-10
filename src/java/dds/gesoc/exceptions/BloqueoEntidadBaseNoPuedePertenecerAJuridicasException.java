acf3ccc389eb70376cc91ba40805f238093fd660
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:41:44 -0300

    Delete BloqueoEntidadBaseNoPuedePertenecerAJuridicasException.java

--- src/java/dds/gesoc/exceptions/BloqueoEntidadBaseNoPuedePertenecerAJuridicasException.java
+++
@@ -1,7 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class BloqueoEntidadBaseNoPuedePertenecerAJuridicasException extends RuntimeException{
-	public BloqueoEntidadBaseNoPuedePertenecerAJuridicasException(String message) {
-		super (message);
-	}
-}
