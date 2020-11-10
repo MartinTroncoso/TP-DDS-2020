92d7e180f201a4a996ede70ef487db3df4d32c92
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:43:06 -0300

    Delete PresupuestoSinEgresoAsociadoException.java

--- src/java/dds/gesoc/exceptions/PresupuestoSinEgresoAsociadoException.java
+++
@@ -1,8 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class PresupuestoSinEgresoAsociadoException extends RuntimeException {
-
-    public PresupuestoSinEgresoAsociadoException(String mensaje) {
-        super(mensaje);
-    }
-}
