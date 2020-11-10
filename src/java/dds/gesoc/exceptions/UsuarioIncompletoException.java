48f8fd9c592bace8005d261c8476289c1df5b2a6
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:43:11 -0300

    Delete UsuarioIncompletoException.java

--- src/java/dds/gesoc/exceptions/UsuarioIncompletoException.java
+++
@@ -1,8 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class UsuarioIncompletoException extends RuntimeException {
-	
-	public UsuarioIncompletoException(String message) {
-		super(message);
-	}
-}
