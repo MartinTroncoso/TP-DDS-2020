8bf2be09a6711f20db91d91dcc63002ec9d6fc5e
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:42:44 -0300

    Delete MetodoHashIncorrectoException.java

--- src/java/dds/gesoc/exceptions/MetodoHashIncorrectoException.java
+++
@@ -1,8 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class MetodoHashIncorrectoException extends RuntimeException {
-	
-	public MetodoHashIncorrectoException(String message) {
-		super(message);
-	}
-}
