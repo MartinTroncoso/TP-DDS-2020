e5378c99344648f3bcb6f8270a9789238f02f9b2
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-11-10 00:42:05 -0300

    Delete ContraseniaConRepetidosSeguidosException.java

--- src/java/dds/gesoc/exceptions/ContraseniaConRepetidosSeguidosException.java
+++
@@ -1,8 +0,0 @@
-package dds.gesoc.exceptions;
-
-public class ContraseniaConRepetidosSeguidosException extends RuntimeException {
-
-	public ContraseniaConRepetidosSeguidosException(String message) {
-		super(message);
-	}
-}
