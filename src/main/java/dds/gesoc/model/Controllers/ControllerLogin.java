bef0d9f6128067cbd991fc0e363646f837940ecd
Marisol <mari967>
2020-11-12 02:09:55 -0300

    Autenticación de usuario

--- src/main/java/dds/gesoc/model/Controllers/ControllerLogin.java
+++ src/main/java/dds/gesoc/model/Controllers/ControllerLogin.java
@@ -1,8 +1,13 @@
 package dds.gesoc.model.Controllers;
 
+import dds.gesoc.model.usuarios.RepoUsuarios;
+import dds.gesoc.model.usuarios.Usuario;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerLogin {
 	public static ModelAndView show(Request req, Response res) {
@@ -11,9 +16,29 @@ public class ControllerLogin {
 	
 	//TODO: CONTROLAR LA CONTRASE�A
 	public static ModelAndView login(Request req, Response res) {
+
+		Map<String, Boolean> model = new HashMap<>();
+		model.put("esInvalido", true);
+
 		String nombre = req.queryParams("usuario");
-        req.session().attribute("usuario-logueado", nombre);
-        res.redirect("/home");
-        return null;
+		String contrasenia = req.queryParams("contrasenia");
+
+		//Buscar usuario
+		Usuario usuario = RepoUsuarios.getInstance().buscarUsuarioPorNombre(nombre);
+
+		if(usuario == null) {
+			return new ModelAndView(model, "/login/login.hbs");
+		}
+		//llamar al método validarContraseña del usuario encontrado
+		boolean resultadoAutenticacion = usuario.autenticarUsuario(contrasenia);
+
+
+		if(resultadoAutenticacion) {
+			req.session().attribute("usuario-logueado", nombre);
+			res.redirect("/home");
+			return null;
+		}
+        //else
+		return new ModelAndView(model, "/login/login.hbs");
 	}
 }
