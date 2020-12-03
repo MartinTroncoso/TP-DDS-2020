956eeadef656a2cb6d85e81ac550757ee594f7ec
MartinTroncoso <49170861+MartinTroncoso@users.noreply.github.com>
2020-12-03 01:30:57 -0300

    Add files via upload

--- src/main/java/Server/Bootstrap.java
+++ src/main/java/Server/Bootstrap.java
@@ -6,11 +6,18 @@ import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
+import dds.gesoc.model.egresos.DireccionPostal;
+import dds.gesoc.model.egresos.Documento;
 import dds.gesoc.model.egresos.Egreso;
+import dds.gesoc.model.egresos.MedioPago;
 import dds.gesoc.model.egresos.Proveedor;
+import dds.gesoc.model.egresos.TipoMedioPago;
+import dds.gesoc.model.geografia.Moneda;
 import dds.gesoc.model.organizaciones.Categoria;
 import dds.gesoc.model.organizaciones.Entidad;
 import dds.gesoc.model.organizaciones.EntidadBase;
+import dds.gesoc.model.usuarios.TipoUsuario;
+import dds.gesoc.model.usuarios.Usuario;
 
 
 public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps{
@@ -21,11 +28,22 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
 	
 	public void init(){
 		withTransaction(() ->{
-			Proveedor proveedor = new Proveedor("Martín");
+			Proveedor proveedor = new Proveedor("Martín","42498956","Argentina", "Cordoba", "Villa Carlos Paz", "Arrecifes 2140");
 			persist(proveedor);
+			
+			Documento documento = new Documento("Tarjeta chica",2703);
+			persist(documento);
+			MedioPago medioPago = new MedioPago(TipoMedioPago.TARJETA_DE_CREDITO);
+			persist(medioPago);
+			Moneda moneda = new Moneda("peso","no para de engordar","$");
+			persist(moneda);
 			
 			Egreso egreso = new Egreso();
 			egreso.setProveedor(proveedor);
+			egreso.setDocComercial(documento);
+			egreso.setMedioPago(medioPago);
+			egreso.setMoneda(moneda);
+			egreso.setCantPresupuestosMinima(3);
 			persist(egreso);
 			
 			Categoria ong = new Categoria("ong");
@@ -38,6 +56,9 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
 			EntidadBase entidadBase = new EntidadBase("juan sa", ong, 2000.00, null);
 			entidadBase.setDescripcion("Organización no Gubernamental");
 			persist(entidadBase);
+			
+			Usuario usuario = new Usuario("Nico","holaquetal",TipoUsuario.ADMINISTRADOR,entidadBase);
+			persist(usuario);
 		});
 	}
 }
--- src/main/java/Server/Router.java
+++ src/main/java/Server/Router.java
@@ -15,15 +15,15 @@ public class Router {
                 .build();
 
 		Spark.staticFiles.location("/public");
-/*		Spark.before((request, response) -> {PerThreadEntityManagers.getEntityManager();});
-*Si el usuario no inició sesión, va a ser redirigido a la página de login
+		Spark.before((request, response) -> {PerThreadEntityManagers.getEntityManager();});
+/*Si el usuario no inició sesión, va a ser redirigido a la página de login
 *				if (!request.pathInfo().contains("/login") &&
 		//			StringUtils.isEmpty(request.cookie("usuario-logueado"))) {
 		//					response.redirect(("/login"));
 		//	}
 *
 *  */
-//		Spark.after((request, response) -> {PerThreadEntityManagers.closeEntityManager();});
+		Spark.after((request, response) -> {PerThreadEntityManagers.closeEntityManager();});
 
 
 
